d3848ee594790903808f626d2898d303925c658e
Princewill Iroka <princewilliroka@outlook.com>
2019-08-25 20:21:24 +0100

    .

--- public/js/app.js
+++ public/js/app.js
@@ -52844,8 +52844,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\NewWebWorkspace\ActiveProjects\medical_equipment_system-laravel - Copy\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\NewWebWorkspace\ActiveProjects\medical_equipment_system-laravel - Copy\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\NewWebWorkspace\ActiveProjects\medical_equipment_system-laravel\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\NewWebWorkspace\ActiveProjects\medical_equipment_system-laravel\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
